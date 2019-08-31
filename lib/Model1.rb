f90772018e884a6a81facdbcec1e645e71b24822
JazzyMussels <jeffmussels@gmail.com>
2019-08-31 15:07:45 -0400

    Done.

--- lib/Model1.rb
+++ lib/Model1.rb
@@ -1,5 +1,37 @@
-#class for Model1 goes here
-#Feel free to change the name of the class
-class Model1
+require 'pry'
+
+class Club
+  attr_accessor :club_name, :type
+  @@all = []
+
+  def initialize(club_name, type)
+    @club_name = club_name
+    @type = type 
+    @@all << self 
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_type(discipline)
+    self.all.select{|club| club.type == discipline}
+  end
+
+  def self.biggest_club
+    self.all.max_by{|club| club.students.length}
+  end
+
+  def members
+    Membership.all.select{|mem| mem.club == self}
+  end
+
+  def students
+    members.map{|mem| mem.student}
+  end
+
+  def number_of_members
+    students.length
+  end
 
 end
--- lib/Model3.rb
+++ lib/Model3.rb
@@ -1,5 +1,23 @@
-#class for Model3 goes here
-#Feel free to change the name of the class
-class Model3
+require 'pry'
+
+class Membership
+  attr_reader :student, :club 
+  attr_accessor :dues
+  @@all = []
+
+  def initialize(dues, student, club)
+    @dues = dues
+    @student = student 
+    @club = club 
+    @@all << self 
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def self.total_dues_collected
+    self.all.map{|mem| mem.dues}.reduce(&:+)
+  end
   
 end
